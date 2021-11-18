9b58b70046d87f203f09f532da67f8e227003682
eds1012 <93288366+eds1012@users.noreply.github.com>
2021-11-18 12:10:41 -0500

    Update CustShip.CreatMassShipDetail.cs

--- SalesOrder_LineHOld/CustShip.CreatMassShipDetail.cs
+++ SalesOrder_LineHOld/CustShip.CreatMassShipDetail.cs
@@ -1,7 +1,7 @@
-This needs to be added as a pre-processing to the Method CustShip.CreateMassShipDetail to block users from shipping an order line that is on hold through the mass shipping function
+//This needs to be added as a pre-processing to the Method CustShip.CreateMassShipDetail to block users from shipping an order line that is on hold through the mass shipping function
 
 
-```
+
 var orderlines = new List<int>();
 var OrderDtl = (from od in Db.OrderDtl
           where od.OrderNum == orderNum
@@ -18,4 +18,4 @@ string ordernumbers = string.Join(", ",unique);
 if (ordernumbers.Length > 0){
  CallContext.Current.ExceptionManager.AddBLException("The following Lines are on hold: "+ordernumbers);
 }
-```
+
